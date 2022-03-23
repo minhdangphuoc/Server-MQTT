9c38336b54c8dab13ac3f2ae0f40dbb7c938dd92
minhdangphuoc <cunminh1405@gmail.com>
2022-03-23 12:41:11 +0200

    First Push

---
+++ main.cpp
@@ -0,0 +1,43 @@
+
+
+
+//////////////////////////////////////  3.3.2022 //////////////////////////////
+///
+///	g++ main.cpp mqtt_receiv.cpp -std=c++0x -pthread -lmosquitto -lmosquittopp -ljsoncpp
+///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <signal.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <unistd.h>
+
+
+using namespace std;
+
+
+#include "mqtt_receiv.h"
+
+///////////////////////////////////////// conficuration ///////////////////////
+
+#include "mqtt.h"
+
+//////////////////////////////////// main /////////////////
+
+
+int main(int argc, char *argv[])
+{	
+	printf ("Raspberry Pi MQTT receive demo\n") ;
+
+	printf ("****   Subscribed to listen MQTT messages \n");
+
+	mqtt mqtt_obj(host_addr, port_number);			// start mqtt 
+
+	while(1);						// keep mqtt send theread running
+						
+	return 0;
+}
