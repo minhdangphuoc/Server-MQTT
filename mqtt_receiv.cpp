086f666caeb9bd177863736925de3c7e88f9c705
minhdangphuoc <cunminh1405@gmail.com>
2022-03-23 13:52:32 +0200

    Add publish

--- mqtt_receiv.cpp
+++ mqtt_receiv.cpp
@@ -41,6 +41,27 @@ mqtt::mqtt(const char *host, int port)						// costructor
 	loop_start();						// stay on mqtt loop
 };
 
+/// Send message
+bool mqtt::send_message(std::string  message)
+
+ {
+ 		// Send message - depending on QoS, mosquitto lib managed re-submission this the thread
+ 		//
+ 		// * NULL : Message Id (int *) this allow to latter get status of each message
+ 		// * topic : topic to be used
+ 		// * lenght of the message
+ 		// * message
+ 		// * qos (0,1,2)
+ 		// * retain (boolean) - indicates if message is retained on broker or not
+ 		// Should return MOSQ_ERR_SUCCESS
+ 
+	int ret = publish(NULL,out_topic,message.length(),message.c_str(),1,false);
+
+	printf(" Message published \n");
+
+ 	return ( ret == MOSQ_ERR_SUCCESS );
+ }
+
 
 ////////// ON CONNECT ///////////////////////////////////////////////////
 
@@ -79,8 +100,10 @@ void mqtt::on_message(const struct mosquitto_message *message)			// on message c
 
 	printf("Message is = %s\n",mqtt_message.c_str()) ;
 	printf("Signal value is = %i\n",mqtt_message[5]- '0') ;
+	
 	tf1.setSignal(mqtt_message[5]- '0');
 	tf1.print();
+	send_message(mqtt_message);
 }  ////////////////////////////         end message received ////////////////////////
 
 
