3d9c7a4f88c3cd61dda6afd9da6d467fbe074625
minhdangphuoc <cunminh1405@gmail.com>
2022-04-19 16:48:00 +0300

    Add Ncurses

--- mqtt_receiv.cpp
+++ mqtt_receiv.cpp
@@ -194,7 +194,7 @@ void mqtt::initWindow()
 	mvwprintw(info_win_1, 0, 1, "Distance");
 	mvwprintw(info_win_2, 0, 1, "Compass");
 	mvwprintw(cmd_win, 0, 1, "Command");
-	wmove(cmd_win, 1, 0);
+	wmove(cmd_win, 1, 1);
 
 	// Refresh
 	refresh();
