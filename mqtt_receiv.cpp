7ccb9965e229118b86ed3085638ef41052fa3b61
minhdangphuoc <cunminh1405@gmail.com>
2022-04-19 13:41:21 +0300

    Add Ncurses

--- mqtt_receiv.cpp
+++ mqtt_receiv.cpp
@@ -86,7 +86,7 @@ void mqtt::on_connect(int rc)							// on connect callback
 
 		subscribe(NULL, in_topic);
 
-		add_log("****   MQTT subscription to topic = " + to_string(in_topic) + "****");
+		add_log("****   MQTT subscription to topic = " + in_topic.c_str() + "****");
 	}
 }
 
